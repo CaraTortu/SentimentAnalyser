2e7c40214f97e5dc1b8fcf470b40aa6604038c3f
CaraTortu <javierdj@jadk.com>
2025-04-22 19:04:43 +0100

    feat: Add classification report and confusion matrix

--- training_model/tests/test.py
+++ training_model/tests/test.py
@@ -1,14 +1,19 @@
 import json
+from collections import Counter
 from typing import Iterable
 
 import gensim.downloader
 import keras
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import seaborn as sns
 from gensim.models import KeyedVectors
 from keras.api.preprocessing.sequence import pad_sequences
 from numpy.typing import NDArray
 from sklearn.metrics import (
-    accuracy_score,
+    classification_report,
+    confusion_matrix,
     mean_absolute_error,
     mean_absolute_percentage_error,
     mean_squared_error,
@@ -72,10 +77,10 @@ def n_to_lbl(n: int) -> int:
     """
     Transforms a number into negative, positive or neutral
     """
-    if n < 0.5:
+    if n < 0.45:
         return NEG
 
-    if n == 0:
+    if n < 0.6:
         return NEU
 
     return POS
@@ -106,6 +111,8 @@ Y_PRED_LBL = map_to_label(Y_pred)
 # GET METRICS #
 ###############
 
+labels = [NEG, NEU, POS]
+class_names = ["Negative", "Neutral", "Positive"]
 
 METRICS = {
     "MSE": mean_squared_error,
@@ -118,5 +125,37 @@ for metric_name, metric_function in METRICS.items():
     result = metric_function(Y, Y_pred)
     print(f"{metric_name}: {result:.4f}")
 
-accuracy = accuracy_score(Y_LBL, Y_PRED_LBL) * 100
-print(f"Accuracy: {accuracy:.2f}%")
+print("Label counter: ", Counter(Y_PRED_LBL), "vs Vader: ", Counter(Y_LBL))
+
+# Classification report
+report = classification_report(
+    Y_LBL, Y_PRED_LBL, labels=labels, target_names=class_names, digits=3
+)
+print("Classification Report:")
+print(report)
+
+##########
+# GRAPHS #
+##########
+
+
+# Generate confusion matrix
+cm = confusion_matrix(Y_LBL, Y_PRED_LBL, labels=labels)
+
+# Normalize it row-wise
+cm_normalized = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+
+# Plot the normalised confusion matrix
+plt.figure(figsize=(6, 5))
+sns.heatmap(
+    cm_normalized,
+    annot=True,
+    fmt=".2f",
+    xticklabels=class_names,
+    yticklabels=class_names,
+    cmap="Blues",
+)
+plt.xlabel("Predicted")
+plt.ylabel("True")
+plt.title("Normalised Confusion Matrix")
+plt.show()
